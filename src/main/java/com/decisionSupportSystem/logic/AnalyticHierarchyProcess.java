850fbb6e408a1312d7e90dd0428bdcddcbdc668a
maximkarpuk <maks131998@mail.ru>
2017-12-13 23:19:19 +0300

    Fix typo

--- src/main/java/com/decisionSupportSystem/logic/AnalyticHierarchyProcess.java
+++ src/main/java/com/decisionSupportSystem/logic/AnalyticHierarchyProcess.java
@@ -82,7 +82,7 @@ public class AnalyticHierarchyProcess {
     }
 
     /**
-     * Multiplies two matrices
+     * Multiplies two matrices.
      */
     private double[] multiplyMatrices(double[][] matrix1, double[][] matrix2) {
         return new double[0];
