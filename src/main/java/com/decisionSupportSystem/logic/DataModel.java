8455ae15ef036126be69320f73cc45355da60091
artoymdmitriev <admitriev5813@gmail.com>
2017-12-16 02:57:39 +0300

    Make controller an observer & add table for comparisons

--- src/main/java/com/decisionSupportSystem/logic/DataModel.java
+++ src/main/java/com/decisionSupportSystem/logic/DataModel.java
@@ -2,8 +2,9 @@ package main.java.com.decisionSupportSystem.logic;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Observable;
 
-public class DataModel {
+public class DataModel extends Observable {
     // Alternatives list
     private ArrayList<Alternative> alternatives = new ArrayList<>();
 
@@ -18,51 +19,29 @@ public class DataModel {
     // Alternatives comparison in each subcriteria
     private HashMap<Criteria, double[][]> alternativesRates = new HashMap<>();
 
-    public ArrayList<Alternative> getAlternatives() {
-        return alternatives;
-    }
-
-    public void setAlternatives(ArrayList<Alternative> alternatives) {
-        this.alternatives = alternatives;
-    }
-
     public ArrayList<Criteria> getCriterias() {
         return criterias;
-    }
-
-    public void setCriterias(ArrayList<Criteria> criterias) {
-        this.criterias = criterias;
-    }
-
-    public double[][] getCriteriasRates() {
-        return criteriasRates;
-    }
-
-    public void setCriteriasRates(double[][] criteriasRates) {
-        this.criteriasRates = criteriasRates;
     }
 
     public HashMap<Criteria, ArrayList<Criteria>> getSubCriterias() {
         return subCriterias;
     }
 
-    public void setSubCriterias(HashMap<Criteria, ArrayList<Criteria>> subCriterias) {
-        this.subCriterias = subCriterias;
+    public void addSubcriteria(Criteria parentCriteria, ArrayList<Criteria> subcriterias) {
+        subCriterias.put(parentCriteria, subcriterias);
+        setChanged();
+        notifyObservers();
     }
 
-    public HashMap<Criteria, double[][]> getSubCriteriasRates() {
-        return subCriteriasRates;
+    public void addSubcriteria(Criteria parentCriteria, Criteria subCriteria) {
+        subCriterias.get(parentCriteria).add(subCriteria);
+        setChanged();
+        notifyObservers();
     }
 
-    public void setSubCriteriasRates(HashMap<Criteria, double[][]> subCriteriasRates) {
-        this.subCriteriasRates = subCriteriasRates;
-    }
-
-    public HashMap<Criteria, double[][]> getAlternativesRates() {
-        return alternativesRates;
-    }
-
-    public void setAlternativesRates(HashMap<Criteria, double[][]> alternativesRates) {
-        this.alternativesRates = alternativesRates;
+    public void addCriteria(Criteria criteria) {
+        criterias.add(criteria);
+        setChanged();
+        notifyObservers();
     }
 }
--- src/main/java/com/decisionSupportSystem/ui/comparisons/ComparisonsController.java
+++ src/main/java/com/decisionSupportSystem/ui/comparisons/ComparisonsController.java
@@ -1,37 +1,93 @@
 package main.java.com.decisionSupportSystem.ui.comparisons;
 
+import com.sun.rowset.internal.Row;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import main.java.com.decisionSupportSystem.logic.Criteria;
 import main.java.com.decisionSupportSystem.logic.DataModel;
-import sun.reflect.generics.tree.Tree;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.ResourceBundle;
+import java.util.*;
 
-public class ComparisonsController implements Initializable {
+public class ComparisonsController implements Initializable, Observer {
     @FXML private TreeView criteriasList;
 
     @FXML private Button addCriteria;
     @FXML private Button addSubcriteria;
     @FXML private Button delete;
+    @FXML private Button addAlternatives;
+
+    @FXML private VBox rightPane;
 
     private DataModel dataModel = new DataModel();
+    private int numberOfAlternatives = 10;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        dataModel.addObserver(this);
+
         TreeItem<Criteria> rootItem = new TreeItem<>(new Criteria("Критерии"));
+        rootItem.setExpanded(true);
         criteriasList.setRoot(rootItem);
+        criteriasList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener() {
+            // creating a table for comparison
+            @Override
+            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+                TreeItem<Criteria> selectedItem = (TreeItem<Criteria>) newValue;
+                if(selectedItem.getChildren().isEmpty()) {
+                    // create an array of textfields
+                    TextField textFields[][] = new TextField[numberOfAlternatives][numberOfAlternatives];
+                    for(int i = 0; i < numberOfAlternatives; i++) {
+                        for(int j = 0; j < numberOfAlternatives; j++) {
+                            textFields[i][j] = new TextField();
+                        }
+                    }
+
+                    // crate pane for textfields
+                    GridPane gridPane = new GridPane();
+
+                    // create constraints for gridPane
+                    for(int i = 0; i < numberOfAlternatives; i++) {
+                        ColumnConstraints colConst = new ColumnConstraints();
+                        colConst.setPercentWidth(100.0 / numberOfAlternatives);
+                        gridPane.getColumnConstraints().add(colConst);
+                    }
+
+                    System.out.println("blebleble");
+                    for(int i = 0; i < numberOfAlternatives; i++) {
+                        RowConstraints rowConst = new RowConstraints();
+                        rowConst.setPercentHeight(100.0 / numberOfAlternatives);
+                        gridPane.getRowConstraints().add(rowConst);
+                    }
+
+                    for(int i = 0; i < numberOfAlternatives; i++) {
+                        for(int j = 0; j < numberOfAlternatives; j++) {
+                            gridPane.add(textFields[i][j], i, j);
+                        }
+                    }
+
+                    rightPane.getChildren().add(gridPane);
+
+                }
+            }
+        });
+
         addCriteria.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
@@ -51,10 +107,7 @@ public class ComparisonsController implements Initializable {
                 inputStage.showAndWait();
 
                 Criteria criteria = new Criteria(loader.<InputController>getController().getFieldValue());
-                TreeItem<Criteria> criteriaTreeItem = new TreeItem<>(criteria);
-                rootItem.getChildren().add(criteriaTreeItem);
-
-                dataModel.getCriterias().add(criteria);
+                dataModel.addCriteria(criteria);
             }
         });
 
@@ -79,26 +132,66 @@ public class ComparisonsController implements Initializable {
                 // Create subcriteria
                 Criteria subcriteria = new Criteria(loader.<InputController>getController().getFieldValue());
                 TreeItem<Criteria> treeItemParentCriteria = (TreeItem<Criteria>) criteriasList.getFocusModel().getFocusedItem();
-                System.out.println(treeItemParentCriteria.toString());
                 Criteria parentCriteria = treeItemParentCriteria.getValue();
-                System.out.println(parentCriteria.toString());
                 subcriteria.setParent(parentCriteria);
 
                 // Add subcriteria to DataModel
                 if(dataModel.getSubCriterias().get(parentCriteria) == null) {
                     ArrayList<Criteria> subcriteriasAL = new ArrayList<>();
                     subcriteriasAL.add(subcriteria);
-                    dataModel.getSubCriterias().put(parentCriteria, subcriteriasAL);
+                    dataModel.addSubcriteria(parentCriteria, subcriteriasAL);
                 } else {
-                    dataModel.getSubCriterias().get(parentCriteria).add(subcriteria);
+                    dataModel.addSubcriteria(parentCriteria, subcriteria);
                 }
-
-                // Add subcriteria to TreeView
-                TreeItem<Criteria> subcriteriaTreeView = new TreeItem<>();
-                subcriteriaTreeView.setValue(subcriteria);
-                treeItemParentCriteria.getChildren().add(subcriteriaTreeView);
-
             }
         });
+
+        addAlternatives.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                // Set up the number of alternatives
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("input.fxml"));
+                Scene newScene;
+                try {
+                    newScene = new Scene(loader.load());
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                    // TODO: handle error
+                    return;
+                }
+
+                Stage inputStage = new Stage();
+                inputStage.initOwner(addCriteria.getScene().getWindow());
+                inputStage.setScene(newScene);
+                inputStage.showAndWait();
+
+                numberOfAlternatives = Integer.parseInt(loader.<InputController>getController().getFieldValue());
+                System.out.println(numberOfAlternatives);
+            }
+        });
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        System.out.println("Hola!");
+        criteriasList.getRoot().getChildren().clear();
+        // add all criterias
+        for(Criteria cr : dataModel.getCriterias()) {
+            TreeItem<Criteria> criteriaTreeItem = new TreeItem<>(cr);
+            criteriasList.getRoot().getChildren().add(criteriaTreeItem);
+        }
+
+        // add all subcriterias
+        for(Map.Entry<Criteria, ArrayList<Criteria>> entry : dataModel.getSubCriterias().entrySet()) {
+            Criteria parentCriteria = entry.getKey();
+            for(Object item : criteriasList.getRoot().getChildren()) {
+                Criteria c = ((TreeItem<Criteria>) item).getValue();
+                if(parentCriteria.getName().equals(c.getName())) {
+                    for(int i = 0; i < entry.getValue().size(); i++) {
+                        ((TreeItem<Criteria>) item).getChildren().add(new TreeItem<>(entry.getValue().get(i)));
+                    }
+                }
+            }
+        }
     }
 }
